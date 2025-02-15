21c707eb02b346de92679ae06bb51da00cab52b4
Shadiyar00 <145849261+Shadiyar00@users.noreply.github.com>
2025-02-16 02:13:21 +0500

    Delete product-list directory

--- product-list/src/app/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { CommonModule } from '@angular/common';
-import { ProductsComponent } from './components/products/products.component';
-import { NgModule } from '@angular/core';
-@NgModule({
-  declarations: [ProductsComponent],
-  imports: [CommonModule], // ✅ Добавляем CommonModule
-})
-export class ProductsModule {}
